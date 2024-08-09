e33ccb7183e8fdb5b04e85d1ea80093809f9e598
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-08-08 21:17:27 -0300

    Cors configurado

--- src/server.js
+++ src/server.js
@@ -12,8 +12,9 @@ const app = express();
 app.use(express.json());
 app.use("/files", express.static(UPLOADS_FOLDER));
 app.use(cookieParser());
-app.use(cors({ 
-  credentials: true
+app.use(cors({     
+    origin: ["*"],
+    credentials: true
 }));
 app.use(routes);
 
